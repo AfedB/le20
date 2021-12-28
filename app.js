a8aa3e8df957bd62fecb6132d2504a79167bd129
AfedB <hafedh@kabaun.com>
2021-12-28 05:38:55 +0100

    commit

---
+++ app.js
@@ -0,0 +1,18 @@
+// const showAnim = gsap.from('.navb', {
+//     yPercent: -100,
+//     paused: true,
+//     duration: 0.2
+// }).progress(1);
+
+// ScrollTrigger.create({
+//     start: "top top",
+//     end: 99999,
+//     onUpdate: (self) => {
+//         self.direction === -1 ? showAnim.play() : showAnim.reverse()
+//     }
+// });
+
+
+
+gsap.from('.li', { duration: 1, opacity: 0, delay: 2, stagger: .5 })
+gsap.from('.hero>a', { duration: 1, opacity: 0, delay: 4 })
